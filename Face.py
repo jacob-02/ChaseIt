6b48bd4b7cfbca2bc5992f2c04f9c696244ed86d
jackjack3006 <jvsanoj23@gmail.com>
2021-05-18 19:42:09 +0530

    Detecting centre of the face

--- Face.py
+++ Face.py
@@ -9,10 +9,13 @@ def start(name):
     a = 0
     while True:
         s, img = vid.read()
+        img = cv2.flip(img, 1)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.circle(img, ((x+x+w)//2, (y+y+h)//2), 7, (0, 255, 0), thickness=cv2.FILLED)
+            print(((x+x+w)//2, (y+y+h)//2))
             a = a + 1
         cv2.imshow(name, img)
         if cv2.waitKey(20) & 0xFF == ord('q'):
