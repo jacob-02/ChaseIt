bdb2bd7dfd5b179029d212de169a34097f4f67f5
jackjack3006 <jvsanoj23@gmail.com>
2021-05-18 17:08:00 +0530

    Face Detection and Recognition done

--- register.py
+++ register.py
@@ -1,23 +1,11 @@
 import os
 import click_img
-import sheet
-
-# a = sheet.write('1', "Sheet1!C1:C2")
 
 
 def reg(data):
     print(data)
     os.mkdir("images/" + data)
     click_img.start(data)
-
-    global a
-    a = sheet.read("Sheet1!C1:C2")
-    print(a[0][0])
-    consumer_column = "Sheet1!A1" + ":A" + str(int(a[0][0]) + 1)
-
-    sheet.write(data, consumer_column)
-
-    consumer_column = int(a[0][0]) + 1
 
 
 name = input("Enter the name of the person ")
