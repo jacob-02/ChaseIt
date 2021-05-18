7d89459fd641713abd5cb6402c13b6d1abaf0e03
jackjack3006 <jvsanoj23@gmail.com>
2021-05-18 19:28:27 +0530

    Face Detection only

--- Face.py
+++ Face.py
@@ -1,42 +1,24 @@
 import cv2
-import pickle
+
+vid = cv2.VideoCapture(0)
 
 
-def reco():
+def start(name):
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    conf_min = 45
-    conf_max = 85
-    recognizer = cv2.face.LBPHFaceRecognizer_create()
-    recognizer.read("trainner.yml")
-    with open("labels.pickle", 'rb') as f:
-        og_labels = pickle.load(f)
-        labels = {v: k for k, v in og_labels.items()}
-    cap = cv2.VideoCapture(0)
 
+    a = 0
     while True:
-        ret, frame = cap.read()
-        print(frame.shape)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        s, img = vid.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            id_, conf = recognizer.predict(roi_gray)
-            if conf_min <= conf <= conf_max:
-                print(id_)
-                print(labels[id_])
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                name = labels[id_]
-                color = (255, 255, 255)
-                stroke = 2
-                cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
-                temp = conf - conf_min
-                cp = 100 - int((temp / conf_min) * 100)
-
-        cv2.imshow('Hello There', frame)
-        if cv2.waitKey(1) == ord('q'):
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            a = a + 1
+        cv2.imshow(name, img)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
             break
-    cap.release()
+    vid.release()
+    cv2.destroyAllWindows()
 
 
-reco()
+start("Hello")
