948cca0932986a7d888edcf64f35a29887c0fed1
jackjack3006 <jvsanoj23@gmail.com>
2021-05-21 11:47:24 +0530

    Upgrading detection to orange

--- Path/pathDetector.py
+++ Path/pathDetector.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 
-def yellow():
+def orange():
     vid = cv2.VideoCapture(0)
     slope = 0.0
 
@@ -12,21 +12,25 @@ def yellow():
     while True:
         ret, image = vid.read()
 
+        # image = cv2.rotate(image, 0)
+        # image = image[190:450, 10:1290]
+
         height = image.shape[0]
         width = image.shape[1]
 
+        image = cv2.GaussianBlur(image, (9, 9), cv2.BORDER_DEFAULT)
         hsvFrame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        yellow_lower = np.array([22, 93, 0], np.uint8)
-        yellow_upper = np.array([45, 255, 255], np.uint8)
-        yellow_mask = cv2.inRange(hsvFrame, yellow_lower, yellow_upper)
+        orange_lower = np.array([5, 50, 50], np.uint8)
+        orange_upper = np.array([25, 255, 255], np.uint8)
+        orange_mask = cv2.inRange(hsvFrame, orange_lower, orange_upper)
 
         kernel = np.ones((5, 5), "uint8")
 
-        yellow_mask = cv2.dilate(yellow_mask, kernel)
-        res_yellow = cv2.bitwise_and(image, image, mask=yellow_mask)
+        orange_mask = cv2.dilate(orange_mask, kernel)
+        res_orange = cv2.bitwise_and(image, image, mask=orange_mask)
 
-        contours, hierarchy = cv2.findContours(yellow_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(orange_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         for pic, contour in enumerate(contours):
             area = cv2.contourArea(contour)
@@ -36,7 +40,7 @@ def yellow():
                               (x + w, y + h),
                               (255, 255, 0), 2)
 
-                cv2.putText(image, "Yellow", (x, y),
+                cv2.putText(image, "Orange", (x, y),
                             cv2.FONT_HERSHEY_SIMPLEX,
                             1.0, (255, 255, 0))
 
@@ -48,11 +52,11 @@ def yellow():
                 if (x - centre_x) != 0:
                     slope = (y - centre_y) / (x - centre_x)
 
-                if slope > 0.2:
+                if slope > 0.1:
                     cv2.putText(image, "Right", (width // 2, height // 2), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255),
                                 3)
 
-                elif slope < -0.2:
+                elif slope < -0.1:
                     cv2.putText(image, "Left", (width // 2, height // 2), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255),
                                 3)
 
@@ -63,7 +67,7 @@ def yellow():
                 coordinates.pop()
                 coordinates.append([centre_x, centre_y])
 
-        cv2.imshow("images", np.hstack([image, res_yellow]))
+        cv2.imshow("images", np.hstack([image, res_orange]))
 
         count += 1
 
