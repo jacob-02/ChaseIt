11ab98f4928e329152be349e22e3759c16cf828b
jackjack3006 <jvsanoj23@gmail.com>
2021-05-22 11:39:25 +0530

    Last minute additions

--- faceDetection/Face.py
+++ faceDetection/Face.py
@@ -5,12 +5,16 @@ def detect(name):
 
     count = 0
     time = 0
+    slope = 0.0
 
     vid = cv2.VideoCapture(0)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default'
                                                                  '.xml')
     while True:
         s, img = vid.read()
+        img = cv2.flip(img, 1)
+        height = img.shape[0]
+        width = img.shape[1]
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.2, 6)
         for (x, y, w, h) in faces:
@@ -18,9 +22,19 @@ def detect(name):
             centre_x, centre_y = ((x + x + w) // 2, (y + y + h) // 2)
             print(centre_x, centre_y)
 
+            if (width // 2 - centre_x) != 0:
+                slope = (height - centre_y) / (width // 2 - centre_x)
+
+            if 4.0 >= slope > 0.0:
+                cv2.putText(img, "Right", (280, 450), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+            elif -0.4 >= slope < 0.0:
+                cv2.putText(img, "Left", (280, 450), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+            else:
+                cv2.putText(img, "Front", (280, 450), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+
         count += 1
 
-        if detector(vid, face_cascade) == 0 or count > 200:
+        if detector(vid, face_cascade) == 0 and count > 200:
             time += 1
             if time > 20:
                 vid.release()
